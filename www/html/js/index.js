1c19ff38d2a8554b73e3b1b0975d1c4cbd007404
nkanematsu <n.kanematsu@gmail.com>
2017-04-15 10:13:38 +0900

    デザイン調整

--- www/html/js/index.js
+++ www/html/js/index.js
@@ -1,9 +1,21 @@
 $(function () {
-    $('.slider').slick({
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 4000,
-        infinite: true,
-        dots: true
-    });
+    $('.slider')
+        .slick({
+            infinite: true,
+            centerMode: true,
+            slidesToShow: 1,
+            centerPadding: '15%',
+            arrows: false,
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            cssEase: 'linear',
+            responsive: [{
+                breakpoint: 640,
+                settings: {
+                    centerMode: false,
+                }
+            }]
+        })
+        .removeClass('hidden');
 });
