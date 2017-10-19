b56b0952ae0cd8a651b50bc8b9d22c9b6f4d06dc
nkanematsu <n.kanematsu@gmail.com>
2017-10-19 20:34:04 +0900

    画像を最適化

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,30 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     watch = require('gulp-watch'),
-    plumber = require('gulp-plumber');
+    plumber = require('gulp-plumber'),
+    imagemin = require('gulp-imagemin'),
+    mozjpeg = require('imagemin-mozjpeg');
 
 gulp.task('sass', function() {
-    gulp.src('assets/sass/**/*.scss')
+    gulp.src('app/assets/stylesheets/**/*.scss')
         .pipe(plumber())
         .pipe(sass())
         .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('imagemin', function() {
+    gulp.src('app/assets/images/**/*')
+        .pipe(plumber())
+        .pipe(imagemin([
+            mozjpeg({
+                quality:85,
+                progressive: true
+            }),
+            imagemin.svgo(),
+            imagemin.optipng(),
+            imagemin.gifsicle()
+        ]))
+        .pipe(gulp.dest('public/img'));
 });
 
 gulp.task('watch', function () {
