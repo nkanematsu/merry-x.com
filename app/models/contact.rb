3104d580bf31a011110cd97ec9f23d80b26b9b35
nkanematsu <n.kanematsu@gmail.com>
2018-03-18 00:15:54 +0900

    お問い合わせのtdkを変更

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,7 +4,6 @@ class Contact < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true
-  validates :corporation, presence: true
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
   validates :body, presence: true
 end
